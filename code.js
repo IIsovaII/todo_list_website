2fc4119402c413b2c6426f029eb237f148f1af76
IIsovaII <164655359+IIsovaII@users.noreply.github.com>
2024-09-10 02:54:25 +0700

    need to add some functions

--- code.js
+++ code.js
@@ -20,7 +20,15 @@ function saveTaskFunc() {
 }
 
 function addTaskFunc() {
-    //
+    let newTask = {}
+    
+    newTask.name = prompt('Task name.', "new task");
+    newTask.description = prompt('Task description.', "");
+    newTask.id = jsonData.tasks.length;
+
+    jsonData.tasks.push(newTask);
+
+    main_workplace();
 }
 
 function editTaskFunc(id, name, desc) {
@@ -166,6 +174,12 @@ function initial_buttons() {
 let main_workplace = () => {
     let workplace = document.getElementById("workplace");
     workplace.innerHTML = "";
+
+    let addBtn = document.createElement("button");
+    addBtn.innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16"><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/></svg>';
+    addBtn.addEventListener("click", () => addTaskFunc());
+    workplace.appendChild(addBtn);
 
     let list_name = document.createElement("h2");
     list_name.innerText = jsonData.name;
